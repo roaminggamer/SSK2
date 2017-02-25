a2539973a4b0b44ae6b56f957a3e20472575d0bf
Roaming Gamer, LLC <roaminggamer@gmail.com>
2017-02-24 22:25:17 -0800

    Updated common colors.

--- releases/ssk2_lite/extras/_blanks/001_basic/scripts/common.lua
+++ releases/ssk2_lite/extras/_blanks/001_basic/scripts/common.lua
@@ -23,11 +23,11 @@ common.playerVelocity 			= 250
 common.playerImpulse 			= 13
 
 -- Colors (player and gate)
-local green							= hexcolor("#4bcc5a")
-local pink							= hexcolor("#d272b4")
-local red							= hexcolor("#ff452d")
-local yellow						= hexcolor("##ffcc00")
-common.colors 						= { green, pink, red, yellow }
+common.green						= hexcolor("#4bcc5a")
+common.pink							= hexcolor("#d272b4")
+common.red							= hexcolor("#ff452d")
+common.yellow						= hexcolor("##ffcc00")
+common.colors 						= { common.green, common.pink, common.red, common.yellow }
 -- Colors (Level and Interface)
 common.backFill1					= hexcolor("#49432b")
 common.backFill2					= hexcolor("#171717")
