e4c416670968a53de5c69202c654e06e6959252f
Roaming Gamer, LLC <roaminggamer@gmail.com>
2018-04-07 14:05:02 -0700

    Minor cleanup.

--- ssk2/loadSSK.lua
+++ ssk2/loadSSK.lua
@@ -34,7 +34,7 @@ local gCount2
 -- Create ssk as global (temporarily)
 _G.ssk = {}
 
-ssk.getVersion = function() return "2018.03.29" end
+ssk.getVersion = function() return "2018.04.02" end
 
 local initialized = false
 ssk.init = function( params )
